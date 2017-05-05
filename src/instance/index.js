31874144bc6d4a4e9e40a882d6b3372e5a9c4740
WittBulter <nanazuimeng123@gmail.com>
2017-05-06 00:35:08 +0800

    修复生命周期的错误

--- src/instance/index.js
+++ src/instance/index.js
@@ -3,30 +3,18 @@
  */
 import {config} from '../constant/config'
 import {bind} from '../compiler/bind'
-import Parse from '../compiler/parse'
-
 import {observer} from './observer'
+
+import Event from './event'
+import Parse from '../compiler/parse'
 import Lifecycle from './lifecycle'
 
 export default class Que {
   constructor(options) {
     this.$options = Object.assign(config, options)
     
-    // 生命周期
     this.$$lifecycle = new Lifecycle(this.$options, this)
     this.$$lifecycle.beforeCreate()
-    
-    // 需要观察的对象
-    // const setProxy = (data, $handle, _this) =>{
-    //   const keys = Object.keys(data)
-    //   let i = keys.length
-    //   while (i --){
-    //     if (typeof data[keys[i]] === 'object'){
-    //       data[keys[i]] = setProxy(data[keys[i]], $handle, _this)
-    //     }
-    //   }
-    //   return observer(data, $handle, _this)
-    // }
     
     this.$scope = observer(this.$options.data, this.$handle, this)
     
@@ -34,6 +22,7 @@ export default class Que {
     this.$watcher = {}
     for (let key in this.$scope){
       this.$watcher[key] = []
+    
     }
     
     this.$$lifecycle.created()
@@ -42,15 +31,16 @@ export default class Que {
       this.$el = el
       this.$$lifecycle.mounted()
     })
+    new Event(this)
   }
   
   $handle(key, _this) {
-    this.$$lifecycle.beforeUpdate()
+    _this.$$lifecycle.beforeUpdate()
     const modelValue = _this.$watcher[key]
     for (let index = 0; index < modelValue.length; index ++){
       modelValue[index].updateNode(_this.$scope)
     }
-    this.$$lifecycle.updated()
+    _this.$$lifecycle.updated()
   }
   
   $destroy() {
@@ -61,34 +51,5 @@ export default class Que {
     }
     this.$$lifecycle.destroyed()
   }
-  
-  $off(eventName, callback) {
-    let fnd = false, removedListeners = []
-    const hasCallback = callback && typeof callback === 'function'
-    if (! this.$$events.length === 0){
-      return
-    }
-    
-    if (! eventName){
-      this.$$events.forEach(v => v.node.removeEventListener(v.mode))
-      return true
-    }
-  
-    this.$$events.forEach((event, i) => {
-      if (eventName === event.mode  && (hasCallback? callback === event.handle: true)){
-        fnd = true
-        event.node.removeEventListener(eventName, event.handle)
-        removedListeners.push(i)
-      }
-    })
-    
-    if (fnd){
-      removedListeners.forEach(v => this.$$events.splice(v, 1))
-    }
-    return fnd
-  }
-  
-  
-  
   
 }
